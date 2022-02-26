19ea685dd0c51aa3007f545bea849e3eaa945d76
Andrey Zakharov <mediaexplorer74@hotmail.com>
2022-02-26 20:13:05 +0300

    .

--- FamilyHub/FamilyAgenda/Utils/Constants.cs
+++ FamilyHub/FamilyAgenda/Utils/Constants.cs
@@ -7,16 +7,15 @@ namespace FamilyAgenda.Utils
     public static class Constants
     {
         #region firebase
-        public const string FirebaseDataBaseUrl = "https://project001-ac43c.firebaseio.com";//"https://familyagenda-9dcc8.firebaseio.com"; // !
-        public const string FirebaseFCMServerKey = "AAAAvyTKOag:APA91bG1TNJBF1U-MM1dOdHWcTgH5NPjJaU-MMuOij536j1LlIGrLImhhmL8pg4RVlby-bSScaOOi5v7KHs7A8V0EBmmXl5rJWOB_hcLbKxly2afmngILMQaq27hvksSiHLWjRP5achfaaj-X1MyJgbXEOZH6p2uFw";//"91173277938"; // !
-        public const string FirebaseBasePostUrl = "https://project001-ac43c.firebaseio.com";//"https://familyagenda-9dcc8.firebaseio.com"; // !
-        public const string FirebaseApiKey = "AIzaSyCrws0V4w2bJP60WSLWQXDWiFOIr6ISG_4";//"familyagenda-9dcc8"; // !
+        public const string FirebaseDataBaseUrl = "https://project001-ac43c.firebaseio.com"; // !
+        public const string FirebaseFCMServerKey = "AAAAvyTKOag:APA91bG1TNJBF1U-MM1dOdHWcTgH5NPjJaU-MMuOij536j1LlIGrLImhhmL8pg4RVlby-bSScaOOi5v7KHs7A8V0EBmmXl5rJWOB_hcLbKxly2afmngILMQaq27hvksSiHLWjRP5achfaaj-X1MyJgbXEOZH6p2uFw";// !
+        public const string FirebaseBasePostUrl = "https://project001-ac43c.firebaseio.com";// !
+        public const string FirebaseApiKey = "AIzaSyCrws0V4w2bJP60WSLWQXDWiFOIr6ISG_4";// !
 
         #endregion
 
         //syncfusion
-        //public const string Old_SyncfusionLicenseKey = "";
-        public const string SyncfusionLicenseKey = "";
+        public const string SyncfusionLicenseKey = ""; // please register your own lic. key
 
         //toast messages
         public const string ConnectivityLostMsg = "No internet connection";
